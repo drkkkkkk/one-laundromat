d2051fb16701c623ab2ce406cde4bfe37858bf7c
drk playz <93538336+drkkkkkk@users.noreply.github.com>
2026-03-11 13:40:16 +0000

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,6 +6,3 @@ this_is_a_map 'yes'
 files {
 'stream/*.ytyp'
 }
-
--- data_file 'DLC_ITYP_REQUEST' 'stream/drk_onelaundro_main.ytyp'
--- data_file 'DLC_ITYP_REQUEST' 'stream/drk_onelaundrodoor.ytyp'
