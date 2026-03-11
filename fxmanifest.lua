39bcc80fb19df083ffd2ca422e937bf10ea17ac5
drk playz <93538336+drkkkkkk@users.noreply.github.com>
2026-03-11 13:32:08 +0000

    Add laundromat files

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+this_is_a_map 'yes'
+
+files {
+'stream/*.ytyp'
+}
+
+-- data_file 'DLC_ITYP_REQUEST' 'stream/drk_onelaundro_main.ytyp'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/drk_onelaundrodoor.ytyp'
